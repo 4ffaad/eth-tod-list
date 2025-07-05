e5162eb7c4d050fabd8359e73fc1f6a6c54e5a92
Daffa <Daffafariq@gmail.com>
2025-07-05 18:06:09 +0800

    Implement smart contract and deploy to Ganache

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,5 +2,23 @@ pragma solidity ^0.5.0;
 
 contract TodoList {
     uint public taskCount = 0;
+
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    // returns the task count -> stores the reference to the task
+    mapping(uint => Task) public tasks;   
+
+    constructor() public {
+        createTask("test");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
 }
 
